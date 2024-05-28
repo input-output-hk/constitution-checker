6a807566e71e7a04f6a81c2ea98143354e4dca15
Bogdan Manole <bogdan.manole@frago.ro>
2024-05-28 12:58:34 +0300

    feat(be): parameters change checker by url

--- backend/src/Cardano/Constitution/Checker/API.hs
+++ backend/src/Cardano/Constitution/Checker/API.hs
@@ -11,20 +11,44 @@ import Cardano.Constitution.Checker.Checks hiding (description)
 
 import Cardano.Constitution.Checker.Types
 import Control.Lens hiding (Context (..), (.=))
-import Data.Swagger as SWG
+import Data.Swagger as SWG hiding (URL)
+import Network.HTTP.Conduit (simpleHttp)
 import Servant.Swagger
 
+import Data.Aeson
+import Data.String
+import Data.Text (unpack)
+import Servant.Client (BaseUrl, parseBaseUrl, showBaseUrl)
+
 type API =
-  "parameters" :> "proposal" :> ReqBody '[JSON] ParametersChange :> Post '[JSON] ParamChecks
-    :<|> "other" :> Get '[JSON] ()
+  "parameters"
+    :> "proposal"
+    :> ( ReqBody '[JSON] ParametersChange :> Post '[JSON] ParamChecks
+          :<|> "by-url" :> ReqBody '[JSON] URL :> Post '[JSON] ParamChecks
+       )
+newtype URL = URL BaseUrl
 
 server :: Server API
 server =
   parametersChange
-    :<|> return ()
+    :<|> parametersChangeByUrl
  where
   parametersChange :: ParametersChange -> Handler ParamChecks
   parametersChange paramChange = pure $ checkParams (mkContext paramChange) paramChange
+  parametersChangeByUrl :: URL -> Handler ParamChecks
+  parametersChangeByUrl url = do
+    param <- fetchParamCheck url
+    parametersChange param
+
+-- TODO: guard checks against exploits
+fetchParamCheck :: URL -> Handler ParametersChange
+fetchParamCheck (URL baseUrl) = do
+  -- Replace with your URL
+  response <- simpleHttp $ showBaseUrl baseUrl
+  let jsonData = eitherDecode' response
+  case jsonData of
+    Right data' -> return data'
+    Left err -> throwError err400{errBody = fromString err}
 
 api :: Proxy API
 api = Proxy
@@ -32,6 +56,23 @@ api = Proxy
 type APIWithDoc =
   SwaggerSchemaUI "swagger-ui" "swagger.json"
     :<|> API
+
+instance ToSchema URL where
+  declareNamedSchema _ = do
+    let
+      schema' =
+        mempty
+          & type_ ?~ SwaggerString
+    return $ NamedSchema (Just "URL") schema'
+
+instance FromJSON URL where
+  parseJSON = withText "URL" $ \t -> do
+    case parseBaseUrl $ unpack t of
+      Just u -> pure $ URL u
+      Nothing -> error "Invalid URL"
+
+instance ToJSON URL where
+  toJSON (URL u) = toJSON $ showBaseUrl u
 
 apiWithDoc :: Proxy APIWithDoc
 apiWithDoc = Proxy
