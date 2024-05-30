ca43c68097b666ee68c6f762a7a3adf1f789b3e0
Bogdan Manole <manoleb@gmail.com>
2024-05-30 13:06:19 +0300

    chore(be): add swagger link information
    Co-authored-by: Romain Soulat <117812549+RSoulatIOHK@users.noreply.github.com>

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -7,4 +7,5 @@ main :: IO ()
 main = do
   let port = 8081
   putStrLn $ "Server running on port " ++ show port
+  putStrLn $ "API swagger available at: " ++ "http://localhost:" ++ show port ++ "/swagger-ui"
   run port app
