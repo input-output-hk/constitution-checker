dc8574d619a31f58e9b7f1aa574ad18bb6afd7e2
Bogdan Manole <bogdan.manole@frago.ro>
2024-06-11 17:22:34 +0200

    fix(be): compilation errors

--- backend/src/Cardano/Constitution/Checker/Params/Definition/MaxBlockHeaderSize.hs
+++ backend/src/Cardano/Constitution/Checker/Params/Definition/MaxBlockHeaderSize.hs
@@ -11,7 +11,6 @@ import Cardano.Constitution.Checker.Params.Swagger ()
 import Cardano.Constitution.Checker.Params.Types
 import Data.Functor.Identity
 import Prelude hiding (Rational)
-import Distribution.Compat.Graph (Node(N))
 
 maxBlockHeaderSize :: Param (Identity Integer)
 maxBlockHeaderSize =
@@ -21,16 +20,16 @@ maxBlockHeaderSize =
     -- 1_100
     [ ("MBHS-01", "maxBlockHeaderSize must not exceed 5,000 Bytes") `MustBe` NG 5_000
     , ("MBHS-02", "maxBlockHeaderSize must not be negative") `MustBe` NL 0
-    , ("MBHS-03", "*maxBlockHeaderSize* **must** be large enough for the largest valid header") `ShouldSatisfy` \ctx val -> 
-      Neutral "Please contribute to the check"
-    , ("MBHS-04", "*maxBlockHeaderSize* **should** only normally be increased if the protocol changes") `ShouldSatisfy` \ctx val -> 
-      case ctx.currentValues.byName.getInteger "maxBlockHeaderSize" of
-        Just maxBlockHeaderSize'
-          | val >= maxBlockHeaderSize' -> Satisfied
-          | otherwise -> Unsatisfied "maxBlockHeaderSize must not be decreased"
-        Nothing -> Unsatisfied "maxBlockHeaderSize not found"
-    , ("MBHS-05", "*maxBlockHeaderSize* **should** be within TCP's initial congestion window (3 or 10 MTUs)") `ShouldSatisfy` \ctx val -> 
-      Neutral "Please contribute to the check"
+    , ("MBHS-03", "*maxBlockHeaderSize* **must** be large enough for the largest valid header") `ShouldSatisfy` \_ _ ->
+        Neutral "Please contribute to the check"
+    , ("MBHS-04", "*maxBlockHeaderSize* **should** only normally be increased if the protocol changes") `ShouldSatisfy` \ctx val ->
+        case ctx.currentValues.byName.getInteger "maxBlockHeaderSize" of
+          Just maxBlockHeaderSize'
+            | val >= maxBlockHeaderSize' -> Satisfied
+            | otherwise -> Unsatisfied "maxBlockHeaderSize must not be decreased"
+          Nothing -> Unsatisfied "maxBlockHeaderSize not found"
+    , ("MBHS-05", "*maxBlockHeaderSize* **should** be within TCP's initial congestion window (3 or 10 MTUs)") `ShouldSatisfy` \_ _ ->
+        Neutral "Please contribute to the check"
     ]
 
 -- Complete as of June 12, 2024
