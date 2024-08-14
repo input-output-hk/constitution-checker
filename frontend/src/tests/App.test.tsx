64a7d9a9155776815505060f67c94d55a6e72064
KJES4 <kathrynjeanelizabethstacy@gmail.com>
2024-08-14 11:20:32 -0500

    add github workflow and pull request files + add 2nd test

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,11 +1,12 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from '../App'; 
-import useStore from '../store'; 
+import App from '../App';
+import useStore from '../store/store'; 
+import { mockInitialJsonState } from './mockData';
 
 describe('Test the useEffect hooks in app component are firing correctly', () => {
   test('fetchJsonInitialState is called once on component mount', async () => {
-    // define the mock function
+    // Define the mock function
     const fetchJsonInitialState = jest.fn();
 
     // Update the mocked store with the mock function
@@ -19,6 +20,30 @@ describe('Test the useEffect hooks in app component are firing correctly', () =>
     // Wait for useEffect to run and ensure fetchJsonInitialState was called
     await waitFor(() => {
       expect(fetchJsonInitialState).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test('postParametersProposal is called when initialJsonState changes', async () => {
+    // Define the mock functions
+    const postParametersProposal = jest.fn();
+
+    // Update the mocked store with the mock function and initial state
+    useStore.setState({
+      postParametersProposal,
+      initialJsonState: undefined, 
+    });
+
+    // Render the component
+    render(<App />);
+
+    // Update state of mock store using the mock data
+    useStore.setState({
+      initialJsonState: mockInitialJsonState,  
+    });
+
+    // Wait for useEffect to run and ensure postParametersProposal was called
+    await waitFor(() => {
+      expect(postParametersProposal).toHaveBeenCalledTimes(1);
     });
   });
 });
